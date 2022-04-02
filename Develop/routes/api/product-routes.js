7d6454b743ae572afc2767b587142ecece1bfd3d
Josh West <bigdaddydas@gmail.com>
2022-04-01 22:05:25 -0600

    tested server

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,20 +1,20 @@
-const router = require("express").Router();
-const { Product, Category, Tag, ProductTag } = require("../../models");
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   // find all products
   Product.findAll({
     include: [
       {
         model: Category,
-        attributes: ["id", "category_name"],
+        attributes: ['id', 'category_name'],
       },
       {
         model: Tag,
-        attributes: ["id", "tag_name"],
+        attributes: ['id', 'tag_name'],
       },
     ],
   })
@@ -26,23 +26,23 @@ router.get("/", (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   // find a single product by its `id`
   Product.findOne({
     include: [
       {
         model: Category,
-        attributes: ["id", "category_name"],
+        attributes: ['id', 'category_name'],
       },
       {
         model: Tag,
-        attributes: ["id", "tag_name"],
+        attributes: ['id', 'tag_name'],
       },
     ],
   })
     .then((dbProductData) => {
       if (!dbProductData) {
-        res.status(404).json({ message: "No product found with this id" });
+        res.status(404).json({ message: 'No product found with this id' });
         return;
       }
 
@@ -55,7 +55,7 @@ router.get("/:id", (req, res) => {
 });
 
 // create new product
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -87,7 +87,7 @@ router.post("/", (req, res) => {
 });
 
 // update product
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -128,9 +128,24 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
-  Product.destroy({});
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
